ae59963323f7e368517f666f460602e96feba84c
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-17 15:13:02 -0800

    Added PriorityQueueTests.

---
+++ PathPlanning/PathPlanningTests/PriorityQueueTests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../PathPlanning/PriorityQueue.h"
+#include "../PathPlanning/PriorityQueue.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PathPlanningTests
+{
+	TEST_CLASS(PriorityQueueTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
