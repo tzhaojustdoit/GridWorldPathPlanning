5a8e7b52fc4617d2c2fb1b667cb5d84f1cba7f74
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-17 14:52:04 -0800

    Implemented Node class.

--- PathPlanning/PathPlanning/Node.cpp
+++ PathPlanning/PathPlanning/Node.cpp
@@ -1,12 +1,73 @@
 #include "Node.h"
 
-
-
-Node::Node()
+int Node::GetG() const
 {
+	return g_;
+}
+
+int Node::GetH() const
+{
+	return h_;
+}
+
+int Node::GetF() const
+{
+	return f_;
+}
+
+Node * Node::GetParent() const
+{
+	return parent_;
+}
+
+int Node::GetNumOfNeighbors() const
+{
+	return numOfNeighbors_;
+}
+
+Node * Node::GetNeighbor(int num) const
+{
+	if (num < numOfNeighbors_) {
+		return neighbors_[num];
+	}
+	return nullptr;
 }
 
 
-Node::~Node()
+
+void Node::SetG(int g)
 {
+	g_ = g;
+	f_ = g_ + h_;
 }
+
+void Node::SetH(int h)
+{
+	h_ = h;
+	f_ = g_ + h_;
+}
+
+void Node::SetParent(Node * n)
+{
+	parent_ = n;
+}
+
+void Node::AddNeightbor(Node * n)
+{
+	neighbors_[numOfNeighbors_] = n;
+	++numOfNeighbors_;
+}
+
+
+bool Node::operator<(const Node &other)
+{
+	if (f_ < other.f_) {
+		return true;
+	}
+	if (f_ > other.f_)
+	{
+		return false;
+	}
+	return g_ > other.g_;
+}
+
--- PathPlanning/PathPlanning/Node.h
+++ PathPlanning/PathPlanning/Node.h
@@ -1,8 +1,34 @@
 #pragma once
+#include <vector>
 class Node
 {
-public:
-	Node();
-	~Node();
-};
 
+public:
+
+	//accessors
+	int GetG() const;
+	int GetH() const;
+	int GetF() const;
+	Node* GetParent() const;
+	int GetNumOfNeighbors() const;
+	Node* GetNeighbor(int) const;
+
+	//mutators
+	void SetG(int);
+	void SetH(int);
+	void SetParent(Node*);
+	void AddNeightbor(Node*);
+
+	//comparators
+	bool operator< (const Node &);
+
+private:
+
+	int g_ = 0;
+	int h_ = 0;
+	int f_ = 0;
+	Node* parent_ = nullptr;
+	int numOfNeighbors_ = 0;
+	Node* neighbors_[4];
+
+};
