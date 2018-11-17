7bc88dc19b38f4b5d4495cd2dc5ba292cb9f022c
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-17 14:53:18 -0800

    Added NodeSetGetTest.

--- PathPlanning/PathPlanningTests/NodeTests.cpp
+++ PathPlanning/PathPlanningTests/NodeTests.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-
+#include "../PathPlanning/Node.h"
+#include "../PathPlanning/Node.cpp"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace PathPlanningTests
@@ -9,9 +10,22 @@ namespace PathPlanningTests
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(NodeSetGetTest)
 		{
-			// TODO: Your test code here
+			Node* n = new Node{};
+			n->SetG(0);
+			n->SetH(1);
+			Assert::AreEqual(0, n->GetG());
+			Assert::AreEqual(1, n->GetH());
+			Assert::AreEqual(1, n->GetF());
+			n->SetG(1);
+			Assert::AreEqual(1, n->GetG());
+			Assert::AreEqual(1, n->GetH());
+			Assert::AreEqual(2, n->GetF());
+			n->SetH(3);
+			Assert::AreEqual(1, n->GetG());
+			Assert::AreEqual(3, n->GetH());
+			Assert::AreEqual(4, n->GetF());
 		}
 
 	};
