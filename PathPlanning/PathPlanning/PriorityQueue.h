59f1c8dff15f9fa7eecfc88e7904ff0e298fde36
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-17 15:24:30 -0800

    Added documentation for PriorityQueue class.

--- PathPlanning/PathPlanning/PriorityQueue.h
+++ PathPlanning/PathPlanning/PriorityQueue.h
@@ -1,15 +1,22 @@
 #pragma once
 #include <vector>
 #include "Node.h"
+// A priority queue implementation, min element is alway on top.
+// @author: Tianhua Zhao
 class PriorityQueue
 {
 public:
 	PriorityQueue();
 	~PriorityQueue();
+	// push a node to priority queue.
 	void push(const Node &);
+	// pop the min node.
 	void pop();
+	// peek the min node.
 	const Node & top() const;
+	// is the priority queue empty?
 	bool empty() const;
+	// the size of the priority queue
 	int size() const;
 private:
 	std::vector<Node> vec_ = std::vector<Node>();
