39a0913d5997daa6e96c21ea8b8a8aaeca3caed7
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-17 15:18:06 -0800

    Implemented PriorityQueue.

--- PathPlanning/PathPlanning/PriorityQueue.cpp
+++ PathPlanning/PathPlanning/PriorityQueue.cpp
@@ -10,3 +10,53 @@ PriorityQueue::PriorityQueue()
 PriorityQueue::~PriorityQueue()
 {
 }
+
+void PriorityQueue::push(const Node &n)
+{
+	vec_.push_back(n);
+	perculate_up(vec_.size() - 1);
+}
+
+void PriorityQueue::pop()
+{
+	std::swap(vec_.front(), vec_.back());
+	vec_.pop_back();
+	perculate_down(0);
+}
+
+const Node & PriorityQueue::top() const
+{
+	return vec_[0];
+}
+
+bool PriorityQueue::empty() const
+{
+	return vec_.size() == 0;
+}
+
+int PriorityQueue::size() const
+{
+	return vec_.size();
+}
+
+void PriorityQueue::perculate_up(int idx)
+{
+	while (idx > 0 && vec_[idx] < vec_[(idx - 1) / 2]) {
+		std::swap(vec_[idx], vec_[(idx - 1) / 2]);
+		idx = (idx - 1) / 2;
+	}
+}
+
+void PriorityQueue::perculate_down(int idx)
+{
+	while (idx * 2 + 1 < (int)vec_.size()) {
+		int minChildIdx = idx * 2 + 1;
+		if (idx * 2 + 2 < (int)vec_.size() && vec_[idx * 2 + 2] < vec_[idx * 2 + 1]) {
+			minChildIdx = idx * 2 + 2;
+		}
+		if (vec_[minChildIdx] < vec_[idx]) {
+			std::swap(vec_[idx], vec_[minChildIdx]);
+		}
+		idx = minChildIdx;
+	}
+}
--- PathPlanning/PathPlanning/PriorityQueue.h
+++ PathPlanning/PathPlanning/PriorityQueue.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <vector>
+#include "Node.h"
 class PriorityQueue
 {
 public:
 	PriorityQueue();
 	~PriorityQueue();
+	void push(const Node &);
+	void pop();
+	const Node & top() const;
+	bool empty() const;
+	int size() const;
+private:
+	std::vector<Node> vec_ = std::vector<Node>();
+	void perculate_up(int);
+	void perculate_down(int);
 };
-
