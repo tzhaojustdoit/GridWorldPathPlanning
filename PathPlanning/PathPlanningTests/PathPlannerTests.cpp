573b6f822dab1cdf0d54f825d1c494f6db6911f8
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-18 23:41:03 -0800

    Added AStarTest.

--- PathPlanning/PathPlanningTests/PathPlannerTests.cpp
+++ PathPlanning/PathPlanningTests/PathPlannerTests.cpp
@@ -12,8 +12,8 @@ namespace PathPlanningTests
 	public:
 		TEST_METHOD_INITIALIZE(Initializer) 
 		{
-			std::vector<char> map_row1{ 's','_' };
-			std::vector<char> map_row2{ 'x','g' };
+			std::vector<char> map_row1{ 's','x' };
+			std::vector<char> map_row2{ '_','g' };
 			map1.push_back(map_row1);
 			map1.push_back(map_row2);
 
@@ -21,13 +21,26 @@ namespace PathPlanningTests
 			map_row2 = { 'x','_' };
 			map2.push_back(map_row1);
 			map2.push_back(map_row2);
+
+			map_row1 = { 'x','_','x','x','_' };
+			map_row2 = { 's','_','_','x','_' };
+			std::vector<char>map_row3 = { 'x','x','_','_','g' };
+			map3.push_back(map_row1);
+			map3.push_back(map_row2);
+			map3.push_back(map_row3);
+
+			map_row1 = { 'x','_','_','_','_' };
+			map_row2 = { 's','_','x','x','_' };
+			map_row3 = { 'x','_','x','_','g' };
+			map4.push_back(map_row1);
+			map4.push_back(map_row2);
+			map4.push_back(map_row3);
 		}
 		TEST_METHOD(ConstructorTest)
 		{
 
 			PathPlanner p{ map1 };
 			// manually debugged
-			Assert::IsTrue(1);
 		}
 
 		TEST_METHOD(StartEndTest)
@@ -46,8 +59,19 @@ namespace PathPlanningTests
 			Assert::IsTrue(p2.get_goal_pos() == expected_goal);
 		}
 
+		TEST_METHOD(AStarTest)
+		{
+
+			PathPlanner p{ map4 };
+			p.a_star_search();
+			std::string res = p.toString();
+			// manualy debugged
+		}
+
 	private:
 		std::vector<std::vector<char>> map1;
 		std::vector<std::vector<char>> map2;
+		std::vector<std::vector<char>> map3;
+		std::vector<std::vector<char>> map4;
 	};
 }
