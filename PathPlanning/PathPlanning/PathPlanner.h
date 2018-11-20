779e8d3a809651c0df76c7014e86550cabfaf98e
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-20 09:27:46 -0800

    Changed PathPlanner to a pure virtual class.

--- PathPlanning/PathPlanning/PathPlanner.h
+++ PathPlanning/PathPlanning/PathPlanner.h
@@ -1,31 +1,21 @@
-#pragma once
+/**
+ * @file PathPlanner.h
+ * @brief Defines the PathPlanner interface.
+ * @authur: Tianhua Zhao
+ */
+
+#ifndef PATH_PLANNER
+#define PATH_PLANNER
+
 #include <vector>
-#include <string>
-#include "PriorityQueue.h"
-#include <stdexcept>
-#include <cmath>
-#include <unordered_map>
-#include "Point2D.h"
 class PathPlanner
 {
 public:
-	PathPlanner();
-	virtual ~PathPlanner();
-	virtual void load_grid_world(std::vector<std::vector<int>> map);
-	virtual void go();
-	virtual int get_num_of_searches();
-	virtual int get_num_of_explored_nodes();
-protected:
-	Point2D get_start_pos() const;
-	Point2D get_goal_pos() const;
-	std::vector<std::vector<int>> input_matrix_;
-	std::vector<std::vector<int>> observed_matrix_;
-	std::vector<std::vector<int>> node_status_matrix_;
-	std::vector<std::vector<Node*>> node_pointer_matrix_;
-	Point2D start_;
-	Point2D goal_;
-	PriorityQueue pq_;
-	void generate(int, int, Node *);
-	void construct_matrices(std::vector<std::vector<int>>);
+	virtual ~PathPlanner() = 0;
+	virtual void LoadGridWorld(std::vector<std::vector<int>> map) = 0;
+	virtual void Go() = 0;
+	virtual int GetNumOfSearches() = 0;
+	virtual int GetNumOfNodesExpanded() = 0;
 };
+#endif
 
