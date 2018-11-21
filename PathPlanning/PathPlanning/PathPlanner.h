4105694c404d4768daa7bee9c3dddb9384577486
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-20 22:38:46 -0800

    Renamed LoadGridWorld to Load.

--- PathPlanning/PathPlanning/PathPlanner.h
+++ PathPlanning/PathPlanning/PathPlanner.h
@@ -12,8 +12,8 @@
 class PathPlanner
 {
 public:
-	virtual ~PathPlanner() = 0;
-	virtual void LoadGridWorld(std::vector<std::vector<char>> map) = 0;
+	virtual ~PathPlanner() {};
+	virtual void Load(const std::vector<std::vector<char>> &map) = 0;
 	virtual void Go() = 0;
 	virtual int GetNumOfSearches() = 0;
 	virtual int GetNumOfNodesExpanded() = 0;
