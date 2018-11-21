f4dbb2a72b8b0b66bb85b408665757b51f50759b
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-20 23:31:35 -0800

    Added code to read data from input file.

--- PathPlanning/PathPlanning/Driver.cpp
+++ PathPlanning/PathPlanning/Driver.cpp
@@ -1,3 +1,30 @@
-int main() {
+#include <vector>
+#include <fstream>
+#include <string>
 
+int main() {
+	std::string filename = "5.txt";
+	std::vector<std::vector<char>> map;
+	std::ifstream input_file_stream(filename);
+	std::string line;
+	if (input_file_stream.good()) {
+		int row;
+		input_file_stream >> row;
+		int col;
+		input_file_stream >> col;
+		map.reserve(row);
+		char cur;
+		for (int i = 0; i < row; i++)
+		{
+			std::vector<char> row_vec;
+			row_vec.reserve(col);
+			for (int i = 0; i < col; i++)
+			{
+				input_file_stream >> cur;
+				row_vec.push_back(cur);
+			}
+			map.push_back(row_vec);
+		}
+		input_file_stream.close();
+	}
 }
