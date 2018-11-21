e1f77574f95f2b75bd454092e9a250c31e602100
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-20 18:14:16 -0800

    Partially implemented AStarPathPlanner class.

--- PathPlanning/PathPlanning/AStarPathPlanner.cpp
+++ PathPlanning/PathPlanning/AStarPathPlanner.cpp
@@ -2,11 +2,133 @@
 
 
 
-AStarPathPlanner::AStarPathPlanner()
-{
-}
-
-
 AStarPathPlanner::~AStarPathPlanner()
 {
 }
+
+// assumption: map has one and only one start and one and only one goal location.
+void AStarPathPlanner::LoadGridWorld(std::vector<std::vector<char>> map)
+{
+	ConstructMatrices(map);
+}
+
+void AStarPathPlanner::Go()
+{
+	// get the start location
+	current_location_ = GetStartLocation();
+	// sense adjacent cells
+	SenseAdjacentCells();
+	// get the goal location
+	goal_location_ = GetGoalLocation();
+
+	while (!(current_location_ == goal_location_))
+	{
+		// plan using the currently observed world map
+		std::stack<Point2D> path_points = Plan();
+		// if path is not found, break out of the loop
+		if (path_points.empty()) {
+			break;
+		}
+		// move the car along the shortest path until it is blocked by an obstacle or reaches the goal
+		while (!path_points.empty())
+		{
+			// sense adjacent cells
+			SenseAdjacentCells();
+			// get location of the next cell
+			Point2D next = path_points.top();
+			int x = current_location_.x;
+			int y = current_location_.y;
+			// if next cell is blocked, break out of the loop
+			if (observed_world_[x][y] == BLOCKED)
+			{
+				break;
+			}
+			// move to the next cell
+			current_location_ = path_points.top();
+			path_points.pop();
+		}
+	}
+	// print number of nodes expanded
+}
+
+
+
+int AStarPathPlanner::GetNumOfSearches()
+{
+	return num_of_searches_;
+}
+
+int AStarPathPlanner::GetNumOfNodesExpanded()
+{
+	return num_of_explored_nodes_;
+}
+
+Point2D AStarPathPlanner::GetStartLocation() const
+{
+	for (unsigned i = 1; i < actual_world_.size() - 1; i++)
+	{
+		for (unsigned j = 1; j < actual_world_[0].size() - 1; j++)
+		{
+			if (actual_world_[i][j] == 's') {
+				return Point2D{ i, j };
+			}
+		}
+	}
+	return Point2D{ -1, -1 };
+}
+
+Point2D AStarPathPlanner::GetGoalLocation() const
+{
+	for (unsigned i = 1; i < actual_world_.size() - 1; i++)
+	{
+		for (unsigned j = 1; j < actual_world_[0].size() - 1; j++)
+		{
+			if (actual_world_[i][j] == 'g') {
+				return Point2D{ i, j };
+			}
+		}
+	}
+	return Point2D{ -1, -1 };
+}
+
+void AStarPathPlanner::SenseAdjacentCells()
+{
+	int x = current_location_.x;
+	int y = current_location_.y;
+	// observe 4 adjacent cells
+	ObserveLocation(x + 1, y);
+	ObserveLocation(x, y + 1);
+	ObserveLocation(x - 1, y);
+	ObserveLocation(x, y - 1);
+}
+
+void AStarPathPlanner::ObserveLocation(int x, int y)
+{
+	char observed = actual_world_[x][y];
+	switch (observed)
+	{
+	case 'x':
+		observed_world_[x][y] = BLOCKED;
+		break;
+	default:
+		observed_world_[x][y] = EMPTY;
+		break;
+	}
+}
+
+void AStarPathPlanner::ConstructMatrices(std::vector<std::vector<char>> map)
+{
+	// num of rows
+	unsigned row = map.size();
+	// num of columns
+	unsigned col = map[0].size();
+
+
+}
+
+void AStarPathPlanner::Plan()
+{
+	num_of_searches_++;
+}
+
+
--- PathPlanning/PathPlanning/A_StarPathPlanner.h
+++ PathPlanning/PathPlanning/A_StarPathPlanner.h
@@ -1,10 +1,60 @@
-#pragma once
+/**
+ * @file PathPlanner.h
+ * @brief Defines the AStarPathPlanner class.
+ * @authur: Tianhua Zhao
+ */
+
+#ifndef A_STAR_PATH_PLANNER
+#define A_STAR_PATH_PLANNER
+
 #include "PathPlanner.h"
+#include "PriorityQueue.h"
+#include "Point2D.h"
+#include "CellType.h"
+#include <stack>
+
 class AStarPathPlanner : public PathPlanner
 {
 public:
-	AStarPathPlanner();
+	AStarPathPlanner() = default;
 	~AStarPathPlanner();
-	void load_map()
+
+	void LoadGridWorld(std::vector<std::vector<char>> map) override;
+	void Go() override;
+	int GetNumOfSearches() override;
+	int GetNumOfNodesExpanded() override;
+
+private:
+	Point2D current_location_;
+	Point2D goal_location_;
+	std::vector<std::vector<char>> actual_world_;
+	std::vector<std::vector<CellType>> observed_world_;
+	//std::vector<std::vector<int>> node_status_matrix_;
+	//std::vector<std::vector<Node*>> node_pointer_matrix_;
+
+	PriorityQueue open_;
+	std::vector<Node*> closed_;
+
+	int num_of_searches_ = 0;
+	int num_of_explored_nodes_ = 0;
+
+	void ConstructMatrices(std::vector<std::vector<char>> map);
+
+	Point2D GetStartLocation() const;
+	Point2D GetGoalLocation() const;
+
+	void SenseAdjacentCells();
+
+	void ObserveLocation(int x, int y);
+
+	void Plan();
+
+	void generate(int, int, Node *);
+
+
+
 };
+#endif // !A_STAR_PATH_PLANNER
+
+
 
