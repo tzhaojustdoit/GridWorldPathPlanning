de6e74d43f3af7e1883ff80025c5ed527ad18a59
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-20 18:13:47 -0800

    Changed the input matrix from a matrix of int to a matrix of char.

--- PathPlanning/PathPlanning/PathPlanner.h
+++ PathPlanning/PathPlanning/PathPlanner.h
@@ -8,14 +8,15 @@
 #define PATH_PLANNER
 
 #include <vector>
+
 class PathPlanner
 {
 public:
 	virtual ~PathPlanner() = 0;
-	virtual void LoadGridWorld(std::vector<std::vector<int>> map) = 0;
+	virtual void LoadGridWorld(std::vector<std::vector<char>> map) = 0;
 	virtual void Go() = 0;
 	virtual int GetNumOfSearches() = 0;
 	virtual int GetNumOfNodesExpanded() = 0;
 };
-#endif
+#endif // !PATH_PLANNER
 
