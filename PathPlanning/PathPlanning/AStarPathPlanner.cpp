b374f36a84f13773ab5967fbaa9be1d2b283467c
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-22 17:48:27 -0800

    Updated Plant().

--- PathPlanning/PathPlanning/AStarPathPlanner.cpp
+++ PathPlanning/PathPlanning/AStarPathPlanner.cpp
@@ -49,7 +49,10 @@ void AStarPathPlanner::Go()
 			std::cout << "No route found" << std::endl;
 			break;
 		}
-		// if path is found, move one step
+		// if path is found, output the navigation map
+		PrintNavigationMap(path_points);
+		// move the car from the start cell to the next cell
+		// since start cell is already sensed, it is safe to move one step
 		current_location_ = path_points.top();
 		path_points.pop();
 		// move the car along the shortest path until it is blocked by an obstacle or reaches the goal
@@ -120,7 +123,7 @@ void AStarPathPlanner::SenseAdjacentCells()
 {
 	int x = current_location_.x;
 	int y = current_location_.y;
-	// observe 4 adjacent cells
+	// observe 4 adjacent cells in this order: right, down, left, up
 	ObserveLocation(x, y + 1);
 	ObserveLocation(x + 1, y);
 	ObserveLocation(x, y - 1);
@@ -147,35 +150,53 @@ void AStarPathPlanner::ObserveLocation(int x, int y)
 
 std::stack<Point2D> AStarPathPlanner::Plan()
 {
-	PriorityQueue open;
-	std::vector<Node*> closed;
-	std::stack<Point2D> path_points;
 	num_of_searches_++;
-	Node* nptr = &observed_world_[current_location_.x][current_location_.y];
-	nptr->set_g(0);
+	std::cout << "Planning with A* (#" << num_of_searches_ << ")" << std::endl;
+	// open list, contains generated nodes
+	PriorityQueue open;
+	// closed list, contains expanded nodes
+	std::vector<Node*> closed;
+	// path points from the current location(exclusive) to the goal location(incluseive)
+	std::stack<Point2D> path_points;
+
+	Node* current_node = &observed_world_[current_location_.x][current_location_.y];
+	// set g value
+	current_node->set_g(0);
 	// set h value
 	int h = current_location_.get_manhattan_distance(goal_location_);
-	nptr->set_h(h);
-	nptr->set_parent(nullptr);
-	open.push(nptr);
+	current_node->set_h(h);
+	// set parent pointer
+	current_node->set_parent(nullptr);
+	// add to the open list
+	open.push(current_node);
 	while (!open.empty())
 	{
 		// move the top of the open list to the closed list
-		nptr = open.top();
+		current_node = open.top();
 		open.pop();
-		closed.push_back(nptr);
-		Point2D location{ nptr->get_x(), nptr->get_y() };
+		// get the current location
+		Point2D location{ current_node->get_x(), current_node->get_y() };
+		// if goal has been reached
 		if (location == goal_location_) {
-			Point2D curr;
-			while (nptr->get_parent() != nullptr)
+			// update h values for nodes in the closed list
+			int g_goal = current_node->get_g();
+			for (Node* var : closed)
 			{
-				curr.x = nptr->get_x();
-				curr.y = nptr->get_y();
+				var->set_h(g_goal - var->get_g());
+			}
+			// make path points, push them to the stack
+			Point2D curr;
+			while (current_node->get_parent() != nullptr)
+			{
+				curr.x = current_node->get_x();
+				curr.y = current_node->get_y();
 				path_points.push(curr);
-				nptr = nptr->get_parent();
+				current_node = current_node->get_parent();
 			}
 			break;
 		}
+		// add the node to the closed list
+		closed.push_back(current_node);
 		// expand the node
 		Expand(location.x, location.y, open, closed);
 	}
@@ -201,10 +222,12 @@ void AStarPathPlanner::Generate(int x, int y, Node* parent, PriorityQueue &open,
 			bool closedcontains = (std::find(closed.begin(), closed.end(), &current_node) != closed.end());
 			if (!open.contains(&current_node) && !closedcontains) {
 				Point2D location{ x,y };
-				int h = location.get_manhattan_distance(goal_location_);
-				current_node.set_h(h);
 				int g = parent->get_g() + 1;
 				current_node.set_g(g);
+				if (current_node.get_h() == 0) {
+					int h = location.get_manhattan_distance(goal_location_);
+					current_node.set_h(h);
+				}
 				current_node.set_parent(parent);
 				open.push(&current_node);
 			}
@@ -217,6 +240,11 @@ void AStarPathPlanner::Generate(int x, int y, Node* parent, PriorityQueue &open,
 	}
 	catch (const std::out_of_range&) {
 	}
+}
+
+void AStarPathPlanner::PrintNavigationMap(std::stack<Point2D>& path_points)
+{
+
 }
 
 
