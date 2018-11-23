45f7f3121a157b88939167f95be83523765c040b
Tianhua Zhao <tzhaojustdoit@me.com>
2018-11-22 23:56:55 -0800

    Fixed documentation.

--- PathPlanning/PathPlanning/Point2D.h
+++ PathPlanning/PathPlanning/Point2D.h
@@ -1,10 +1,43 @@
-#pragma once
+/**
+ * @file Point2D.h
+ * @brief Defines the Point2D struct.
+ * @authur: Tianhua Zhao
+ */
+
+#ifndef POINT_2D_
+#define POINT_2D_
+
 #include <cmath>
+
+/**
+ * @struct Point2D
+ *
+ * @brief a 2d point.
+ */
 struct Point2D
 {
+	/**
+	 * @brief x coordinate
+	 */
 	int x;
+
+	/**
+	 * @brief y coordinate
+	 */
 	int y;
+
+	/**
+	 * @brief get the manhattan distance to another 2d point
+	 */
 	int get_manhattan_distance(const Point2D &other) const;
+
+	/**
+	 * @brief overloaded == operator
+	 */
 	bool operator==(const Point2D &other) const;
 };
+
+#endif // !POINT_2D_
+
+
 
