f81ddc404fb835be57b18d4c981ec4e9ecde6d63
Tianhua Zhao <tzhaojustdoit@me.com>
2018-12-30 12:34:32 -0800

    Added more documentation.

--- PathPlanning/PathPlanning/Display.h
+++ PathPlanning/PathPlanning/Display.h
@@ -9,6 +9,14 @@
 #include "Marker.h"
 
 namespace Display {
+
+	/**
+	 * @brief display the planned navigation map.
+	 * @param row number of rows of the map
+	 * @param map contains obsacle info of the map
+	 * @param path contains path info of the map
+	 * @param start, goal contains start adn goal location of the map
+	 */
 	void DisplayMap(int row, int col, const std::vector<bool> &map, const std::vector<int> &path, int start, int goal);
 }
 #endif // !DISPLAY_
